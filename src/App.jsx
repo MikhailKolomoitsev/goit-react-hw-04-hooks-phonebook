3c28ee621b79673b8d512a532129218be8251921
mihon.testosterone <mihon.testosterone@gmail.com>
2021-09-07 14:28:41 +0300

    smth

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,20 @@
 import "./App.css";
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Form from "./components/Form/Form";
 import Contacts from "./components/Contacts/Contacts";
 import Filter from "./components/Filter/Filter";
 // import { v4 as uuidv4 } from "uuid";
 
-class App extends Component {
-  static defaultProps = {
-    contacts: [],
-  };
-  static propTypes = {
-    contacts: PropTypes.array.isRequired,
-  };
+function App() {
 
-  state = {
-    contacts: [
+ const [contacts, setContacts] = useState([
       { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
       { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
       { id: "id-3", name: "Eden Clements", number: "645-17-79" },
       { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-    ],
-    filter: "",
-  };
+    ])
+const [filter, setFilter] = useState('')
 
   componentDidMount() {
     const contacts = localStorage.getItem("contacts");
@@ -85,5 +77,9 @@ class App extends Component {
     );
   }
 }
+
+propTypes = {
+    contacts: PropTypes.array.isRequired,
+  };
 
 export default App;
