c5217368294b14c18e9c91c75d2f818a6aa5473e
mihon.testosterone <mihon.testosterone@gmail.com>
2021-09-10 11:33:19 +0300

    code fixes

--- src/components/ListContacts/ListContacts.jsx
+++ src/components/ListContacts/ListContacts.jsx
@@ -17,48 +17,10 @@ export default function ListContacts({contacts, onClick}) {
 }
   
 ListContacts.propTypes = {
-    data: PropTypes.oneOfType([
+    contacts: PropTypes.oneOfType([
       PropTypes.string,
       PropTypes.number,
       PropTypes.array,
     ]),
     onClick: PropTypes.func.isRequired,
   };
-
-//  class oldContacts extends Component {
-//   static defaultProps = {
-//     contacts: [],
-//   };
-
-//   static propTypes = {
-//     contacts: PropTypes.oneOfType([
-//       PropTypes.string,
-//       PropTypes.number,
-//       PropTypes.array,
-//     ]),
-//     onClick: PropTypes.func.isRequired,
-//   };
-
-//   state = { contacts: this.props.contacts };
-
-//   handleClick = (id) => {
-//     this.props.onClick(id);
-//   };
-
-//   render() {
-//     const data = this.props.contacts;
-//     return (
-//       <ul>
-//         {data.map((contact) => (
-//           <li key={contact.id}>
-//             {contact.name} {contact.number}
-//             <button onClick={() => this.handleClick(contact.id)} type="button">
-//               delete
-//             </button>
-//           </li>
-//         ))}
-//       </ul>
-//     );
-//   }
-// }
-
